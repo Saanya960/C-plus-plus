e378e431fb303449083d7743481acb200668d43d
Saanya Mittal <m.saanyak@gmail.com>
2026-06-07 01:04:40 +0530

    Initial C++ programs repository

---
+++ Patterns/a.cpp
@@ -0,0 +1,18 @@
+//Nested loop
+//incomplete
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    for(int i=1;i<=4;i++){
+        for(int j=i;j<=4;j++){
+            cout<<j;
+        }
+       cout<<endl;
+    }
+
+    return 0;
+}
